c11a7ff7c0b4a2c82701058516bdcadb6efd1c13
Lory <mentamaliciosa@gmail.com>
2019-06-21 18:21:22 +0200

    con animación

--- app/src/main/java/es/popapps/trivisk/MainActivity.java
+++ app/src/main/java/es/popapps/trivisk/MainActivity.java
@@ -4,7 +4,9 @@ import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -13,10 +15,10 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    ImageView imgPlay;
-    Button buttonSettings;
+    ImageView imgLogo;
+    Button btnSettings, btnPlay, btnCreate, btnBuy;
     Dialog dialog;
     Typeface lucky;
     TextView tvSound, tvMusic, tvSettings;
@@ -27,9 +29,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSettings = findViewById(R.id.btnSettings);
+        btnBuy = findViewById(R.id.btnBuy);
+        btnCreate = findViewById(R.id.btnCreate);
+        btnPlay = findViewById(R.id.btnPlay);
+        imgLogo = findViewById(R.id.imgLogo);
 
-        imgPlay = findViewById(R.id.btnPlay);
-        buttonSettings = findViewById(R.id.btnSettings);
+        btnCreate.setOnClickListener(this);
+        btnBuy.setOnClickListener(this);
+        btnPlay.setOnClickListener(this);
+
+        ((AnimationDrawable) imgLogo.getBackground()).start();
+
+ /*     Esto lo metemos en el switch case
 
         imgPlay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,8 +50,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-
-        buttonSettings.setOnClickListener(new View.OnClickListener() {
+*/
+        btnSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showDialogPreferences();
@@ -63,8 +75,9 @@ public class MainActivity extends AppCompatActivity {
 
         seekSound.setOnSeekBarChangeListener (new SeekBar.OnSeekBarChangeListener() {
 
+            @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                tvSound.setText("" + progress + " %");
+                tvSound.setText(" " + progress + " %");
             }
 
             @Override
@@ -82,8 +95,9 @@ public class MainActivity extends AppCompatActivity {
 
         seekMusic.setOnSeekBarChangeListener (new SeekBar.OnSeekBarChangeListener() {
 
+            @SuppressLint("SetTextI18n")
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                tvMusic.setText("" + progress + " %");
+                tvMusic.setText(" " + progress + " %");
             }
 
             @Override
@@ -101,5 +115,24 @@ public class MainActivity extends AppCompatActivity {
             dialog.show();
 
     }
+
+
+    @Override
+    public void onClick(View v) {
+        int aver = v.getId();
+        switch (aver) {
+            case R.id.btnBuy:
+                Snackbar.make(v, "hola btnBuy", Snackbar.LENGTH_LONG).show();
+            break;
+            case R.id.btnCreate:
+                Snackbar.make(v, "hola btnCreate", Snackbar.LENGTH_LONG).show();
+            break;
+            case R.id.btnPlay:
+                Snackbar.make(v, "hola btnPlay", Snackbar.LENGTH_LONG).show();
+            break;
+            default:
+    }
+}
+
 
 }
